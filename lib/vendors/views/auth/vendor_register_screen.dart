fa89ec2888abd2402b134f5efc9c250b08a0bc46
adogbeji <baka_1993@yahoo.co.uk>
2024-08-24 09:40:58 +0100

    Add starting code for vendor register screen

---
+++ lib/vendors/views/auth/vendor_register_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VendorRegisterScreen extends StatelessWidget {
+  const VendorRegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Vendor Register Screen'),
+      ),
+    );
+  }
+}
