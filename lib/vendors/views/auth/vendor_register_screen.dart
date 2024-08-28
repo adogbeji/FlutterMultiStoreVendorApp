9b2132c3c78d8b0b330dfc7b436aa111dbc81fc0
adogbeji <baka_1993@yahoo.co.uk>
2024-08-28 09:47:46 +0100

    Add gradient background & camera icon to app bar of cendor registration screen

--- lib/vendors/views/auth/vendor_register_screen.dart
+++ lib/vendors/views/auth/vendor_register_screen.dart
@@ -5,12 +5,45 @@ class VendorRegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             backgroundColor: Colors.pink,
             toolbarHeight: 200,
+            flexibleSpace: LayoutBuilder(builder: (context, constraints) {
+              return FlexibleSpaceBar(
+                background: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.yellow.shade900,
+                        Colors.yellow,
+                      ],
+                    ),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 90,
+                          width: 90,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.camera_alt),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
           ),
         ],
       ),
