6629a08bbcbb68ce9e2bdf3f062a4d9a89a94840
adogbeji <baka_1993@yahoo.co.uk>
2024-09-02 09:16:24 +0100

    Install image_picker package & create function to pick image from gallery or camera

--- lib/vendors/controllers/vendor_register_controller.dart
+++ lib/vendors/controllers/vendor_register_controller.dart
@@ -1,1 +1,17 @@
-class VendorController {}
+import 'package:image_picker/image_picker.dart';
+
+class VendorController {
+
+  // Picks image from phone gallery or device camera
+  pickStoreImage(ImageSource source) async {
+    final ImagePicker _imagePicker = ImagePicker();  // Stores image_picker package
+
+    XFile? _file = await _imagePicker.pickImage(source: source);  // Stores picked image
+
+    if (_file != null) {
+      return await _file.readAsBytes();
+    } else {
+      print('No Image Selected!');
+    }
+  }
+}
