d530f8db9b26be783537720fd36afda8135393e0
adogbeji <baka_1993@yahoo.co.uk>
2024-09-09 09:42:39 +0100

    Create new function in VendorController class

--- lib/vendors/controllers/vendor_register_controller.dart
+++ lib/vendors/controllers/vendor_register_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 
 class VendorController {
@@ -13,5 +14,40 @@ class VendorController {
     } else {
       print('No Image Selected!');
     }
+  }
+
+  Future<String> registerVendor(
+    String businessName, 
+    String email, 
+    String phoneNumber, 
+    String countryValue, 
+    String stateValue, 
+    String cityValue, 
+    String taxOptions, 
+    String taxNumber,
+    Uint8List? image
+  ) async {
+    String res = 'Some error occured';
+
+    try {
+      if (businessName.isNotEmpty &&
+          email.isNotEmpty &&
+          phoneNumber.isNotEmpty &&
+          countryValue.isEmpty &&
+          stateValue.isNotEmpty &&
+          cityValue.isNotEmpty &&
+          taxOptions.isNotEmpty &&
+          taxNumber.isNotEmpty &&
+          image != null
+      ) {
+        // Save data to Firestore Database
+      } else {
+        res = 'Fields must not be empty';
+      }
+    } catch (e) {
+      res = e.toString();
+    }
+
+    return res;
   }
 }
